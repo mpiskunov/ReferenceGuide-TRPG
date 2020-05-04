f244178801eebe0a35bc7f7f889a335459e724d6
Gering <GDong01@wildcats.jwu.edu>
2020-05-04 00:13:09 -0400

    fixed the armor routing issue

--- src/app/items/general-items/general-items.component.ts
+++ src/app/items/general-items/general-items.component.ts
@@ -23,7 +23,7 @@ export class GeneralItemsComponent extends TableBuilderComponent implements OnIn
 
     let url = route.url.subscribe({
       next: url => {
-        debugger;
+        //debugger;
         this.callBackUrl = url[0].path;
       }
     });
