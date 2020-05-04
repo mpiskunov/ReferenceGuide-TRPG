dd13399064b98010218330a501cd06ce5135d9db
Gering <GDong01@wildcats.jwu.edu>
2020-05-04 00:30:03 -0400

    created an armor interface

---
+++ src/app/armor/armor-list/armor.ts
@@ -0,0 +1,16 @@
+export interface IArmor{
+    id: number;
+    name: string;
+    stoppingPower: number;
+    defaultReliability: number;
+    availability: number;
+    enhancementSlots: number;
+    encumberanceValue: number;
+    weight: number;
+    cost:number;
+    description: string;
+    weightClassification: number;
+    deleted: true
+    sourceID: number;
+     
+}
