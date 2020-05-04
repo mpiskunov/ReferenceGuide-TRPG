f4eb525266c6d421d6d26b8aa88a1da27cdf38c5
Gering <GDong01@wildcats.jwu.edu>
2020-05-04 01:16:47 -0400

    working on displaying armor list

---
+++ src/app/armor/armor-list/armor.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {ArmorListComponent} from './armor-list.component'
+
+
+@NgModule({
+    declarations:[ArmorListComponent],
+    imports:[ 
+        
+    ]
+
+})
